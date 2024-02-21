5ecc0d591896aae3be3ae1da41fc0c15671b8962
pedroddvo <pedroddvo@gmail.com>
2024-02-21 11:45:27 +0000

    precompileModules

--- Test.lean
+++ Test.lean
@@ -3,5 +3,4 @@ import C
 def main : IO Unit := do
   let x : C.Int8 := (C.Int16.ofNat 255).toInt8
   let y : UInt8 := (-13 : C.Int16) |>.toUInt16.toUInt8
-  def z := ((32 - 64) : C.Int32)
-   IO.println s!"{y}"
+  IO.println s!"{y}"
--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,16 @@
 import Lake
-open Lake DSL
+open Lake DSL System
 
 package «clean» where
   -- add package configuration options here
+
+lean_lib «C» where
+  precompileModules := true
+
+@[default_target]
+lean_exe «test» where
+  root := `Test
+  supportInterpreter := true
 
 target ffi.o pkg : FilePath := do
   let oFile := pkg.buildDir / "C" / "ffi.o"
@@ -14,15 +22,3 @@ extern_lib libleanffi pkg := do
   let name := nameToStaticLib "ffi"
   let ffiO ← fetch <| pkg.target ``ffi.o
   buildStaticLib (pkg.nativeLibDir / name) #[ffiO]
-
-@[default_target]
-lean_lib «C» where
-  -- add library configuration options here
-
--- @[default_target]
-lean_exe «test» where
-  root := `Test
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
